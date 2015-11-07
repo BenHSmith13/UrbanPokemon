325186536e65c32e33fa40072e58dcf3ad085328
dittonjs <jditton.atomic@gmail.com>
2015-11-06 18:40:41 -0700

    adds player movement

---
+++ UrbanPokemon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float xDir;
+	public float yDir;
+
+	void Awake () {
+		xDir = 0f;
+		yDir = 0f;
+	}
+
+
+	void Update () {
+		Move ();
+	}
+
+	void Move () {
+
+		this.yDir = Input.GetAxis("Vertical");
+		this.xDir = Input.GetAxis("Horizontal");
+		transform.Translate(this.xDir * speed * Time.deltaTime, this.yDir * speed * Time.deltaTime, 0f);
+	}
+
+}
