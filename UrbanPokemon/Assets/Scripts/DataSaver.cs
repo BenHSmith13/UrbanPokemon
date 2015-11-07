3bb4cd79cae0a3c3363027e12528b5d42be4b861
dittonjs <jditton.atomic@gmail.com>
2015-11-06 19:34:33 -0700

    adds basic type definitions

---
+++ UrbanPokemon/Assets/Scripts/DataSaver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Collections;
+using UnityEngine;
+
+public static class DataScript{
+	public static void Save(PlayerData data){
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/GameData.dat");
+		if(data != null){
+			bf.Serialize(file, data);
+		}
+		file.Close();
+	}
+	
+	public static PlayerData Load(){
+		if (File.Exists(Application.persistentDataPath + "/GameData.dat")) {
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/GameData.dat", FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize(file);
+			file.Close();
+			return data;
+		}
+		return new PlayerData();
+	}
+}
+
+[Serializable]
+public class PlayerData {
+	public float playerXPos;
+	public float playerYPos;
+	public SimpleMonster[] monsters;
+	public int money;
+	public string name;
+	public SimpleTrophy[] trophies;
+
+}
+
---
+++ UrbanPokemon/Assets/Scripts/SimpleGameObjectTypes.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMonster {
+	public string name;
+	public int level;
+	public int totalExp;
+	public SimpleMove[] moves;
+	public string type;
+}
+
+public class SimpleMove {
+	public int baseAttack;
+	public int accuracy;
+	public string name;
+	public string type;
+}
+
+public class SimpleNPC {
+	public string name;
+	public string conversationText;
+}
+
+public class SimpleBattleNpc {
+	public string name;
+	public string startBattleText;
+	public string finishBattleText;
+	public SimpleMonster[] monsters;
+	public int moneyGiven;
+}
+
+public class SimpleTrophy { // probably should call this something else
+	public string city;
+}
+
+public class SimpleItem {
+	public int buyCost;
+	public int sellCost;
+	public string name;
+	public string type;
+}
+
+
