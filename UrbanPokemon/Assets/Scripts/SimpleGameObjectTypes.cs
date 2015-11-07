68b3a92323bea5a149f798ded2f0965cb23d1cd0
dittonjs <jditton.atomic@gmail.com>
2015-11-06 20:51:31 -0700

    addes loading and saving data

--- UrbanPokemon/Assets/Scripts/SimpleGameObjectTypes.cs
+++ UrbanPokemon/Assets/Scripts/SimpleGameObjectTypes.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
 
+[Serializable]
 public class SimpleMonster {
 	public string name;
 	public int level;
 	public int totalExp;
-	public SimpleMove[] moves;
+	public List <SimpleMove> moves;
 	public string type;
+	public string status;
 }
 
+[Serializable]
 public class SimpleMove {
 	public int baseAttack;
 	public int accuracy;
@@ -16,28 +23,39 @@ public class SimpleMove {
 	public string type;
 }
 
+[Serializable]
 public class SimpleNPC {
 	public string name;
 	public string conversationText;
+	public int sceneNumber;
+	public float xPos;
+	public float yPos;
 }
 
+[Serializable]
 public class SimpleBattleNpc {
 	public string name;
 	public string startBattleText;
 	public string finishBattleText;
-	public SimpleMonster[] monsters;
+	public List <SimpleMonster> monsters;
 	public int moneyGiven;
+	public int sceneNumber;
+	public float xPos;
+	public float yPos;
 }
 
+[Serializable]
 public class SimpleTrophy { // probably should call this something else
 	public string city;
 }
 
+[Serializable]
 public class SimpleItem {
 	public int buyCost;
 	public int sellCost;
 	public string name;
 	public string type;
+	public void use(){}
 }
 
 
