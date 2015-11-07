be145e5ae0e352bf99f3bd9d93cbbfe823dcd130
dittonjs <jditton.atomic@gmail.com>
2015-11-06 23:59:08 -0700

    adds animation to guy

--- UrbanPokemon/Assets/Scripts/PlayerController.cs
+++ UrbanPokemon/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,15 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float xDir;
 	public float yDir;
-
+	public Sprite up;
+	public Sprite down;
+	public Sprite left;
+	public Sprite right;
+	public Animator animator;
 	void Awake () {
 		xDir = 0f;
 		yDir = 0f;
+		animator = gameObject.GetComponent<Animator>();
 	}
 
 
@@ -18,10 +23,35 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Move () {
-
 		this.yDir = Input.GetAxis("Vertical");
 		this.xDir = Input.GetAxis("Horizontal");
+		if(this.xDir > 0) {
+
+			gameObject.GetComponent<SpriteRenderer>().sprite = right;
+		}
+//		if(this.xDir < 0) {
+//			gameObject.GetComponent<SpriteRenderer>().sprite = left;
+//		}
+//		if(this.yDir > 0) {
+//			gameObject.GetComponent<SpriteRenderer>().sprite = up;
+//		}
+//		if(this.yDir < 0) {
+//			gameObject.GetComponent<SpriteRenderer>().sprite = down;
+//		}
 		transform.Translate(this.xDir * speed * Time.deltaTime, this.yDir * speed * Time.deltaTime, 0f);
+	}
+
+	void SetRight(){
+
+	}
+	void SetLeft(){
+
+	}
+	void SetUp(){
+
+	}
+	void SetDown(){
+
 	}
 
 }
